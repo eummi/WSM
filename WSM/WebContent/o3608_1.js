1944ecd6cf84843c7783bf39586b0c9392198f1c
Eummi Park <mep5245@naver.com>
2019-05-19 21:20:46 +0900

    json, xml 작성 완료

---
+++ WSM/WebContent/o3608_1.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+	var interval = setInterval(handleRefresh, 3000);
+    handleRefresh();
+}
+
+function handleRefresh() {
+    console.log("here");
+
+	var url = "http://openapi.seoul.go.kr:8088/416565744d6d657036374e536e4141/json/TB_PUBLIC_BCYCL_LEND_USE/1/10/";
+	$.getJSON(url, updateRent);
+}
+
+/*
+1	LEND_LC	대여소위치
+2	LEND_NM	대여소명
+3	BASIC_DT	기준일자
+4	LEND_CNT	대여건수
+5	RTURN_CNT	반납건수
+*/
+
+function updateRent(bicyclerent) {
+	var bicycleDiv = document.getElementById("bicyclerent");
+	bicyclerent = bicyclerent.TB_PUBLIC_BCYCL_LEND_USE.row;
+
+	for (var i = 0; i < bicyclerent.length; i++) {
+		var rent = bicyclerent[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "rent");
+		div.innerHTML = "대여소위치:"+rent.LEND_LC
+		 +" 대여소명:"+rent.LEND_NM + " "
+		 +" 기준일자:"+rent.BASIC_DT + " "
+		 +" 대여건수:"+rent.LEND_CNT + " "
+		 +" 반납건수:"+rent.RTURN_CNT;
+
+		bicycleDiv.appendChild(div);
+	}
+
+}
---
+++ WSM/WebContent/o3608_2.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+    handleRefresh();
+}
+
+function handleRefresh() {
+
+	var url = "http://openapi.seoul.go.kr:8088/416565744d6d657036374e536e4141/xml/TB_PUBLIC_BCYCL_LEND_USE/1/100/";
+
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		updateRent(this);
+    	}
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+/*
+1	LEND_LC	대여소위치
+2	LEND_NM	대여소명
+3	BASIC_DT	기준일자
+4	LEND_CNT	대여건수
+5	RTURN_CNT	반납건수
+*/
+
+function updateRent(xml) {
+	var xmlDoc = xml.responseXML;
+	var bicycleDiv = document.getElementById("bicyclerent");
+	bicyclerent = xmlDoc.getElementsByTagName("row");
+
+	for (var i = 0; i < bicyclerent.length; i++) {
+		var row = bicyclerent[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "rent");
+		div.innerHTML = "대여소위치:" + row.getElementsByTagName("LEND_LC")[0].childNodes[0].nodeValue + "은 "
+        + " 대여소명:" + row.getElementsByTagName("LEND_NM")[0].childNodes[0].nodeValue 
+        + " 기준일자:" + row.getElementsByTagName("BASIC_DT")[0].childNodes[0].nodeValue 
+        + " 대여건수:" + row.getElementsByTagName("LEND_CNT")[0].childNodes[0].nodeValue 
+        + " 반납건수:" + row.getElementsByTagName("RTURN_CNT")[0].childNodes[0].nodeValue;
+
+		bicycleDiv.appendChild(div);
+	}
+
+}
